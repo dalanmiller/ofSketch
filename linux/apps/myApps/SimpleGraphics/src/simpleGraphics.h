c954863bf52b0f40edc8bdcfccfc9102b5e6ede0
Steve Minor <stv.mnr@gmail.com>
2012-10-26 07:21:56 -0700

    added example from programming interactivity

---
+++ linux/apps/myApps/SimpleGraphics/src/simpleGraphics.h
@@ -0,0 +1,12 @@
+#pragma mark once
+
+#include "ofMain.h"
+
+class simpleGraphics : public ofBaseApp{
+    public:
+    void setup();
+    void update();
+    void draw();
+
+    ofColor fillColor;
+};
