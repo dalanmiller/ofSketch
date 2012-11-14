d67edbaf6e2bb7b461e6488620dac6227384fa3d
Steve Minor <stv.mnr@gmail.com>
2012-11-13 19:01:55 -0800

    got basic save functionality working

--- wishServer/requests_test.py
+++ wishServer/requests_test.py
@@ -11,27 +11,8 @@ db = connection.dandelion
 #db.collection_names() #this shows that dandelion is the collection that we want
 collection = db.dandelion #set the collection
 
-print collection
-
-#print r.json["results"]
-
-#some debugging
-#print "Number of results: "
-#print len(r.json["results"])
-#print "Result type"
-#print type(r.json["results"])
-#print "*" * 30 + "\n\n"
-
 for tweet in r.json["results"]:
-	#add a wishGranted parameter
-	tweet["granted"] = false;
-
 	#save the record in the database if no result is returned
-	#this is called an upsert and I don't know how to accomplish it
-
-	collection.insert(tweet) #to be replaced with upsert
-	#save into database
-#	print "type(tweet)"
-#	for t in tweet:
-#		print "tweet[" + unicode(t) + "] : " + unicode(tweet[t])
-#	print "*" * 30	
+	if collection.find_one({'id' : tweet["id"]}) is not None:
+		tweet["granted"] = false; #add a wishGranted parameter
+		collection.insert(tweet) #to be replaced with upsert
